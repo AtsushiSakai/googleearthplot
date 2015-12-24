b00e76b385a581d869323bf8df87f455627b29a6
AtsushiSakai <asakai.amsl+github@gmail.com>
2015-12-24 15:58:38 +0900

    add gitinore and setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "googleearthplot",
+    version = "0.0.1",
+    author = "Atsushi Sakai",
+    author_email = "asakai.amsl@gmail.com",
+    description = ("KML file generator for plotting on Google Earth"),
+    license = "MIT",
+    keywords = "google earth plot kml",
+    url = "https://github.com/AtsushiSakai/googleearthplot",
+    packages=find_packages(),
+    install_requires = [],
+)
